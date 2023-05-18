d0b258eafc3554bf88e995a136576baef318de61
MaxSavostyanov <maxsavostyanov34@gmail.com>
2023-05-18 14:08:00 +0300

    refact: убрать дублирование верстки IngredientDatails

--- src/components/ingredient-details/ingredient-details.jsx
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -10,10 +10,10 @@ export default function IngredientDetails({ isBackground }) {
   const { id } = useParams();
   const ingredient = ingredients.find((ingredient) => ingredient._id === id);
 
-  const IngredientDetailsModal = () => {
+  const IngredientDetailsElement = ({ container, title }) => {
     return ingredient ? (
-      <div className={`${styles.container} pb-5`}>
-        <h2 className={`${styles.title} text text_type_main-large pt-4 pb-5`}>
+      <div className={`${container} pb-5`}>
+        <h2 className={`${title} text text_type_main-large pt-4 pb-5`}>
           Детали ингредиента
         </h2>
 
@@ -67,65 +67,12 @@ export default function IngredientDetails({ isBackground }) {
     ) : null;
   }
 
-  const IngredientDetailsRoute = () => {
-    return ingredient ? (
-      <div className={`${styles.containerRoute} pb-5`}>
-        <h2 className={`${styles.titleRouter} text text_type_main-large pt-4 pb-5`}>
-          Детали ингредиента
-        </h2>
-
-        <img
-          src={ingredient.image_large}
-          alt={ingredient.name}
-        />
-
-        <h3 className='text text_type_main-medium pt-4 pb-4'>
-          {ingredient.name}
-        </h3>
-
-        <ul className={`${styles.list} pt-4 pb-5`}>
-          <li className={`${styles.item}`}>
-            <p className='text text_type_main-default text_color_inactive pb-2'>
-              Калории,ккал
-            </p>
-            <p className='text text_type_main-default text_color_inactive'>
-              {ingredient.calories}
-            </p>
-          </li>
-
-          <li className={`${styles.item}`}>
-            <p className='text text_type_main-default text_color_inactive pb-2'>
-              Белки, г
-            </p>
-            <p className='text text_type_main-default text_color_inactive'>
-              {ingredient.proteins}
-            </p>
-          </li>
-
-          <li className={`${styles.item}`}>
-            <p className='text text_type_main-default text_color_inactive pb-2'>
-              Жиры, г
-            </p>
-            <p className='text text_type_main-default text_color_inactive'>
-              {ingredient.fat}
-            </p>
-          </li>
-
-          <li className={`${styles.item}`}>
-            <p className='text text_type_main-default text_color_inactive pb-2'>
-              Углеводы, г
-            </p>
-            <p className='text text_type_main-default text_color_inactive'>
-              {ingredient.carbohydrates}
-            </p>
-          </li>
-        </ul>
-      </div>
-    ): null;
-  }
-
   return isBackground
-    ? <IngredientDetailsModal />
-    : <IngredientDetailsRoute />
+    ? <IngredientDetailsElement
+      container={styles.container}
+      title={styles.title} />
+    : <IngredientDetailsElement
+      container={styles.containerRoute}
+      title={styles.titleRouter}
+    />
 }
-
